c08d02b35bb459af8d3e47f833bcae3c9680e8ec
dungnt <iamleader157@gmail.com>
2025-05-11 21:00:29 +0700

    add entity for movie services

---
+++ services/movie-service/src/main/java/vn/ptit/moviebooking/entity/Cinema.java
@@ -0,0 +1,53 @@
+package vn.ptit.moviebooking.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "cinema")
+@DynamicInsert
+@DynamicUpdate
+public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "name", length = 100, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
