c31ac565e9e2bccba32363887972d3cfef659a39
NguyenPhong2612 <nguyendacphong0307@gmail.com>
2025-05-11 20:40:09 +0700

    add api get customer by id

--- services/common-service/src/main/java/vn/ptit/moviebooking/CommonApplication.java
+++ services/common-service/src/main/java/vn/ptit/moviebooking/CommonApplication.java
@@ -3,12 +3,7 @@ package vn.ptit.moviebooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-    }
-)
+@SpringBootApplication()
 public class CommonApplication {
 
     public static void main(String[] args) {
