7d419081fa2143a2f8f9b411f3d69899660593c1
thoaidc <dcthoai1023@gmail.com>
2025-05-11 23:09:42 +0700

    add api gateway and service discovery server

---
+++ gateway/src/main/java/vn/ptit/moviebooking/GatewayApplication.java
@@ -0,0 +1,11 @@
+package vn.ptit.moviebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
