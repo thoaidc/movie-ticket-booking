9c2b12749c10e3021be1a8e33a024de0d49cea1d
thoaidc <dcthoai1023@gmail.com>
2025-05-11 19:33:17 +0700

    add base exception for common service

---
+++ services/common-service/src/main/java/vn/ptit/moviebooking/exception/BaseBadRequestAlertException.java
@@ -0,0 +1,12 @@
+package com.dct.nextgen.exception;
+
+public class BaseBadRequestAlertException extends BaseException {
+
+    public BaseBadRequestAlertException(String entityName, String errorKey) {
+        super(entityName, errorKey, null, null);
+    }
+
+    public BaseBadRequestAlertException(String entityName, String errorKey, Object[] args, Throwable error) {
+        super(entityName, errorKey, args, error);
+    }
+}
