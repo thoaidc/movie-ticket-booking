e57571cd97cb23d2f3842dd4fd1c8b7328f7af89
thoaidc <dcthoai1023@gmail.com>
2025-05-11 21:13:15 +0700

    update config pom

--- services/common-service/src/main/java/vn/ptit/moviebooking/CommonApplication.java
+++ services/common-service/src/main/java/vn/ptit/moviebooking/CommonApplication.java
@@ -3,7 +3,7 @@ package vn.ptit.moviebooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class CommonApplication {
 
     public static void main(String[] args) {
