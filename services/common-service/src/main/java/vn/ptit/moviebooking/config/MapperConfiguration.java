6b0ab34c6acc6d7619babf2717d65fb0654652d1
thoaidc <dcthoai1023@gmail.com>
2025-05-11 10:39:44 +0700

    add constants and common config

---
+++ services/common-service/src/main/java/vn/ptit/moviebooking/config/MapperConfiguration.java
@@ -0,0 +1,30 @@
+package vn.ptit.moviebooking.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Avoid errors when encountering undefined properties in JSON that are not present in the Java class being converted
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // Do not serialize (convert to JSON) fields with empty or null values
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        objectMapper.registerModule(new JavaTimeModule()); // To support Instant datatype of Java 8
+        objectMapper.registerModule(new Jdk8Module());
+
+        return objectMapper;
+    }
+}
