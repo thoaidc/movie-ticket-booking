9816d2fff87ff2672e8ded0afdbaf0406dc90a86
thoaidc <dcthoai1023@gmail.com>
2025-05-12 17:27:41 +0700

    update api gateway config and add service register client

--- services/payment-service/src/main/java/vn/ptit/moviebooking/payment/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/vn/ptit/moviebooking/payment/PaymentServiceApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
 import vn.ptit.moviebooking.payment.config.CRLFLogConverter;
@@ -13,6 +14,7 @@ import java.net.UnknownHostException;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     private static final Logger log = LoggerFactory.getLogger(PaymentServiceApplication.class);
